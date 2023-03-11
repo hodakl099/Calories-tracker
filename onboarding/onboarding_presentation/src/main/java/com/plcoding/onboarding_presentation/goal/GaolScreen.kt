8e4af3378dedbf59453209ad905d0bdbb8f19400
mahmo <mahmoud.alkateb22@gmail.com>
2023-03-11 12:35:43 +0200

    GoalScreen.kt and GoalViewModel.kt set up

---
+++ onboarding/onboarding_presentation/src/main/java/com/plcoding/onboarding_presentation/goal/GaolScreen.kt
@@ -0,0 +1,94 @@
+package com.plcoding.onboarding_presentation.goal
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.plcoding.core.domain.model.ActivityLevel
+import com.plcoding.core.domain.model.Gender
+import com.plcoding.core.domain.model.GoalType
+import com.plcoding.core.util.UiEvent
+import com.plcoding.core_ui.LocalSpacing
+import com.plcoding.onboarding_presentation.R
+import com.plcoding.onboarding_presentation.components.ActionButton
+import com.plcoding.onboarding_presentation.components.SelectableButton
+import kotlinx.coroutines.flow.collect
+
+@Composable
+fun ActiviScreen(
+    onNavigate : (UiEvent.Navigate) -> Unit,
+    viewModel: GoalViewModel = hiltViewModel()
+) {
+    val spacing = LocalSpacing.current
+    LaunchedEffect(key1 = true) {
+        viewModel.uiEvent.collect {  event ->
+            when(event){
+                is UiEvent.Navigate -> onNavigate(event)
+                else -> Unit
+            }
+        }
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(spacing.spaceLarge)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = stringResource(id = R.string.whats_your_activity_level)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            Row {
+                SelectableButton(
+                    text = stringResource(id = R.string.low),
+                    isSelected = viewModel.selectedGoalType is GoalType.LoseWeight,
+                    color = MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = Color.White,
+                    onClick = {
+                        viewModel.onGoalTypeSelect( GoalType.LoseWeight)
+                    },
+                    textStyle = MaterialTheme.typography.button.copy(
+                        fontWeight = FontWeight.Normal
+                    )
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceMedium))
+                SelectableButton(
+                    text = stringResource(id = R.string.medium),
+                    isSelected = viewModel.selectedGoalType is GoalType.KeepWeight,
+                    color = MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = Color.White,
+                    onClick = {
+                        viewModel.onGoalTypeSelect(GoalType.KeepWeight)
+                    }
+                )
+                Spacer(modifier = Modifier.width(spacing.spaceMedium))
+                SelectableButton(
+                    text = stringResource(id = R.string.high),
+                    isSelected = viewModel.selectedGoalType is GoalType.GainWeight,
+                    color = MaterialTheme.colors.primaryVariant,
+                    selectedTextColor = Color.White,
+                    onClick = {
+                        viewModel.onGoalTypeSelect(GoalType.GainWeight)
+                    }
+                )
+
+            }
+        }
+        ActionButton(
+            text = stringResource(id = R.string.next),
+            onClick = viewModel::onNextClick,
+            modifier = Modifier.align(Alignment.BottomEnd)
+        )
+    }
+}
